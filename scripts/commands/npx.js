7923c569a597c9ef7f89796d0521b04d8c566f8e
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-04-26 23:37:49 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -1,31 +1,27 @@
-const fs = require("fs");
-module.exports = {
-  config:{
-	name: "😒",
-        version: "1.0.1",
-        prefix: false,
-	permssion: 0,
-	credits: "nayan", 
-	description: "Fun",
-	category: "no prefix",
-	usages: "😒",
-        cooldowns: 5, 
-},
+	const fs = require("fs");
+module.exports.config = {
+	    name: "😍",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😍",
+    cooldowns: 5,
+};
 
-handleEvent: function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
+	if (event.body.indexOf("amogus")==0 || event.body.indexOf("😍")==0 || event.body.indexOf("❤️")==0 || event.body.indexOf("🥰")==0) {
 		var msg = {
-				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
+				body: "এ্ঁতো্ঁ ভা্ঁলো্ঁবা্ঁসা্ঁ ক্ঁই্ঁ পা্ঁও্ঁ 🥰",
+				attachment: fs.createReadStream(__dirname + `/noprefix/valobasa.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🥺", event.messageID, (err) => {}, true)
 		}
-	},
-	start: function({ nayan }) {
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
-}
