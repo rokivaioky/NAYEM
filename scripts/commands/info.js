5cec161abc288b90bb44dbe8ed07d73a17a81591
rokivaioky <168263243+rokivaioky@users.noreply.github.com>
2024-04-27 14:06:54 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,15 +28,23 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐅𝐚𝐫𝐡𝐚𝐧 𝐈𝐬𝐥𝐚𝐦
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : 𝐌𝐝 𝐍𝐚𝐲𝐞𝐦 𝐊𝐡𝐚𝐧𝐝𝐨𝐤𝐞𝐫😈
+😾🫦
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
- 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐁𝐚𝐫𝐢𝐬𝐡𝐚𝐥
+🫰😘
+ 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐏𝐓𝐊.
+ 😈❤️
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+😔🥵
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+🥺💋
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+😐🤦‍♂️
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐈𝐧𝐛𝐨𝐱
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100069926588234&mibextid=b06tZ0`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+😝🙈
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/NAYEM.KING.OK`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100069926588234/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://i.postimg.cc/0QxyKgYW/inbound4628190817202335261.jpg`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
